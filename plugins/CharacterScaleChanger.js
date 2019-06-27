63ec231a21ad8adca1572d99986fb0f0a76a6758
Tsumio <32388898+Tsumio@users.noreply.github.com>
2019-06-27 09:14:03 +0900

    Update CharacterScaleChanger.js

--- plugins/CharacterScaleChanger.js
+++ plugins/CharacterScaleChanger.js
@@ -6,6 +6,7 @@
 // http://opensource.org/licenses/mit-license.php
 // ----------------------------------------------------------------------------
 // Version
+// 1.1.0 2019/06/27 Scaleの変更にシンボル名を指定できるようにした。
 // 1.0.0 2019/06/26 公開。
 // ----------------------------------------------------------------------------
 // [GitHub] : https://github.com/Tsumio/rmmv-plugins
@@ -29,20 +30,52 @@
  * 
  * ----how to use----
  * After introducing this plugin, use the script command.
+ * If you want to change scale only for events that have a specific memo tag specified
+ * it is necessary to set up the memo field.
+ * The specification method of this memo field is as follows.
+ * <scale:distinguishedName>
+ * 
+ * For example, if you want to distinguish by "MiniCharacter", write as follows in the event memo field.
+ * <scale:MiniCharacter>
+ * 
+ * It is possible to specify the same for multiple events.
+ * If multiple events are specified, multiple events are affected by scale change at the same time.
  * 
  * ----scripts----
- * -> Change of character size
+ * -> Change of all character size
  * $gameSystem.changeCharacterScale(ScaleX, ScaleY);
  * Ex: Increase both horizontal and vertical 1.5 times
  * $gameSystem.changeCharacterScale(1.5, 1.5);
  * 
- * -> Get currently scaleX
+ * -> Get currently all scaleX
  * $gameSystem.getCharacterScaleX();
  * 
- * -> Get currently scaleY
+ * -> Get currently all scaleY
  * $gameSystem.getCharacterScaleY();
  * 
+ * -> Change the scale only for events where a specific memo tag is specified
+ * $gameSystem.changeCharacterScale(ScaleX, ScaleY, 'Symbol');
+ * Example: Scale of the event group for which the "miniCharacter" is specified in the memo filed.
+ * $gameSystem.changeCharacterScale(0.5, 0.5, 'miniCharacter');
+ * 
+ * ----symbol----
+ * In the script $gameSystem.changeCharacterScale(scaleX, scaleY, 'symbol')
+ * If you do not specify a Symbol Name, "all" is implicitly specified.
+ * Furthermore, to specify characters other than events, specify the following symbol names.
+ * 
+ * -> Player character: player
+ * -> Follower: follower
+ * -> Vehicle: vehicle
+ * 
+ * Also, do not use "null" and "all" for symbol name.
+ * 
+ * ----priority----
+ * The priority of scale change is
+ * 1.Scale specified by individual symbol name
+ * 2.Implicitly specified all (no symbol name specified)
+ * 
  * ----change log---
+ * 1.1.0 2019/06/27 It became possible to specify a symbol name in the change of Scale.
  * 1.0.0 2019/06/26 Release.
  * 
  * ----remarks----
@@ -71,20 +104,53 @@
  * 
  * 【使用方法】
  * プラグインの導入後、スクリプトコマンドから使用します。
+ * また、特定のメモタグが指定されているイベントだけScaleを変更したい場合
+ * メモ欄の設定が必要になります。
+ * このメモ欄の指定方法は以下の通りです。
+ * <scale:区別名>
+ * 
+ * 例えば「ミニキャラ」で区別したい場合、イベントのメモ欄に以下のように記述します。
+ * <scale:ミニキャラ>
+ * 
+ * この区別名は複数のイベントに同じものを指定することが可能です。
+ * 複数のイベントに指定した場合、複数のイベントが同時にScale変更の影響を受けます。
  * 
  * 【スクリプト】
- * ・キャラクターサイズの変更
+ * ・キャラクターサイズの一括変更
  * $gameSystem.changeCharacterScale(横のScale, 縦のScale);
- * 例：横も縦も1.5倍にする
+ * 例：横も縦も一括して1.5倍にする
  * $gameSystem.changeCharacterScale(1.5, 1.5);
  * 
- * ・現在設定している横のScaleの取得
+ * ・現在設定している一括変更分の横のScaleの取得
  * $gameSystem.getCharacterScaleX();
  * 
- * ・現在設定している縦のScaleの取得
+ * ・現在設定している縦の一括変更分のScaleの取得
  * $gameSystem.getCharacterScaleY();
  * 
+ * ・特定のメモタグが指定されているイベントだけScaleを変更
+ * $gameSystem.changeCharacterScale(横のScale, 縦のScale, '区別名');
+ * 例：メモ欄でミニキャラが指定されたイベント郡のScaleを縦も横も0.5倍する
+ * $gameSystem.changeCharacterScale(0.5, 0.5, 'ミニキャラ');
+ * 
+ * 【区別名】
+ * $gameSystem.changeCharacterScale(横のScale, 縦のScale, '区別名')のスクリプトにおいて
+ * 区別名の指定を省略した場合は暗黙的に「all」が指定されたことになります。
+ * また、イベント以外のキャラクターを指定するには以下の区別名を指定します。
+ * 
+ * ・プレイヤーキャラクター：player
+ * ・フォロワー：follower
+ * ・乗り物：vehicle
+ * 
+ * また、区別名には「null」と「all」を使用しないでください。
+ * 
+ * 【優先順位】
+ * Scale変更の優先順位は
+ * 1．個別の区別名で指定したサイズ
+ * 2．暗黙的に指定されたall（区別名の指定なし）
+ * の順になります。
+ * 
  * 【更新履歴】
+ * 1.1.0 2019/06/27 Scaleの変更にシンボル名を指定できるようにした。
  * 1.0.0 2019/06/26 公開。
  * 
  * 【備考】
@@ -183,36 +249,94 @@
     const _Game_System_initialize = Game_System.prototype.initialize;
     Game_System.prototype.initialize = function() {
         _Game_System_initialize.call(this);
-        this._characterScale = {x:1, y:1};
+        this._characterScaleDict = {};
+    };
+
+    //本当は変更してほしくない。イミュータブルにしたいが……。
+    Game_System.prototype.characterScaleDict = function() {
+        if(!this._characterScaleDict) {
+            this._characterScaleDict = {};
+        }
+        return this._characterScaleDict;
     };
 
     //スケールの値の保存と適用を同時におこなう
-    Game_System.prototype.changeCharacterScale = function(scaleX = 1, scaleY = 1) {
-        this._setCharacterScale(scaleX, scaleY);
-        this._applyCharacterScale();
+    Game_System.prototype.changeCharacterScale = function(scaleX = 1, scaleY = 1, symbol = Game_Scale.ALL_SYMBOL) {
+        this._setCharacterScale(scaleX, scaleY, symbol);
+        this._applyCharacterScale(symbol);
     };
 
     //XのScaleを取得する
-    Game_System.prototype.getCharacterScaleX = function() {
-        return this._characterScale ? this._characterScale.x : 1;
+    Game_System.prototype.getCharacterScaleX = function(symbol = Game_Scale.ALL_SYMBOL) {
+        const target = this.characterScaleDict()[symbol];
+        return target ? target.x : 1;
     };
 
     //YのScaleを取得する
-    Game_System.prototype.getCharacterScaleY = function() {
-        return this._characterScale ? this._characterScale.y : 1;
+    Game_System.prototype.getCharacterScaleY = function(symbol = Game_Scale.ALL_SYMBOL) {
+        const target = this.characterScaleDict()[symbol];
+        return target ? target.y : 1;
     };
 
     //Scaleの変更を適用する
-    Game_System.prototype._applyCharacterScale = function() {
+    Game_System.prototype._applyCharacterScale = function(symbol = Game_Scale.ALL_SYMBOL) {
         if(SceneManager._scene._spriteset) {
-            SceneManager._scene._spriteset.setCharacterScale(this.getCharacterScaleX(), this.getCharacterScaleY());
+            const scaleX = this.getCharacterScaleX(symbol);
+            const scaleY = this.getCharacterScaleY(symbol);
+            SceneManager._scene._spriteset.setCharacterScale(scaleX, scaleY, symbol);
         }
     };
 
     //Scaleの変更をModelに保存する
-    Game_System.prototype._setCharacterScale = function(scaleX = 1, scaleY = 1) {
-        this._characterScale.x = scaleX;
-        this._characterScale.y = scaleY;
+    Game_System.prototype._setCharacterScale = function(scaleX = 1, scaleY = 1, symbol = Game_Scale.ALL_SYMBOL) {
+        const target = this.characterScaleDict()[symbol];
+        if(!target) {
+            this.characterScaleDict()[symbol] = new Game_Scale(scaleX, scaleY, symbol);
+        }
+        this.characterScaleDict()[symbol].set(scaleX, scaleY);
+    };
+
+////=============================================================================
+//// Game_Character
+////  Scaleの設定のためのメモ欄取得機能を基底クラスに追加する
+////=============================================================================
+
+    //各子クラスでオーバーライドすることを想定
+    Game_Character.prototype.scaleMeta = function() {
+        return 'null';
+    };
+
+////=============================================================================
+//// Game_Event
+////  Scaleの設定のためのメモ欄取得機能を追加
+////=============================================================================
+    Game_Event.prototype.scaleMeta = function() {
+        return this.event().meta[Game_Scale.META_NAME] || 'null';
+    };
+
+////=============================================================================
+//// Game_Player
+////  Scaleの設定のためのメモ欄取得機能を追加
+////=============================================================================
+
+    Game_Player.prototype.scaleMeta = function() {
+        return 'player';
+    };
+
+////=============================================================================
+//// Game_Follower
+////  Scaleの設定のためのメモ欄取得機能を追加
+////=============================================================================
+    Game_Follower.prototype.scaleMeta = function() {
+        return 'follower';
+    };
+
+////=============================================================================
+//// GameVehicle
+////  Scaleの設定のためのメモ欄取得機能を追加
+////=============================================================================
+    Game_Vehicle.prototype.scaleMeta = function() {
+        return 'vehicle';
     };
 
 ////=============================================================================
@@ -224,14 +348,93 @@
     const _Spriteset_Map_createCharacters = Spriteset_Map.prototype.createCharacters;
     Spriteset_Map.prototype.createCharacters = function() {
         _Spriteset_Map_createCharacters.call(this);
-        const scaleX = $gameSystem.getCharacterScaleX();
-        const scaleY = $gameSystem.getCharacterScaleY();
-        this.setCharacterScale(scaleX, scaleY);
+        //最初にAllSymbolのスケールを変更
+        this._changeOnlyAllSymbolSprite();
+        //その後、AllSymbol以外の別のシンボルを全て変更
+        this._changeAllCharacterScaleExceptAllSymbol();
     };
 
     //Scaleの変更をSpriteに直接セットする
-    Spriteset_Map.prototype.setCharacterScale = function(scaleX, scaleY) {
-        this._characterSprites.forEach(sprite => sprite && sprite.scale.set(scaleX, scaleY));
+    Spriteset_Map.prototype.setCharacterScale = function(scaleX, scaleY, symbol) {
+        if(symbol === Game_Scale.ALL_SYMBOL) {
+            this._applyCharacterScale(this._characterSprites, scaleX, scaleY);
+        }else {
+            const targetSpriteList = this._characterSprites.filter(sprite => sprite && sprite._character.scaleMeta() === symbol);
+            this._applyCharacterScale(targetSpriteList, scaleX, scaleY);
+        }
     };
+
+    Spriteset_Map.prototype._applyCharacterScale = function(targetSpriteList, scaleX, scaleY) {
+        targetSpriteList.forEach(sprite => sprite && sprite.scale.set(scaleX, scaleY));
+    };
+
+    //Game_Scale.ALL_SYMBOLのみを変更する
+    Spriteset_Map.prototype._changeOnlyAllSymbolSprite = function() {
+        const scaleX = $gameSystem.getCharacterScaleX();
+        const scaleY = $gameSystem.getCharacterScaleY();
+        this.setCharacterScale(scaleX, scaleY, Game_Scale.ALL_SYMBOL);
+    };
+
+    //Game_Scale.ALL_SYMBOL以外のシンボルを全て変更適用する
+    Spriteset_Map.prototype._changeAllCharacterScaleExceptAllSymbol = function() {
+        const allKeyList = Object.getOwnPropertyNames($gameSystem.characterScaleDict());
+        const keyList    = allKeyList.filter(key => key !== Game_Scale.ALL_SYMBOL);
+        keyList.forEach(key => {
+            const scaleMoedl = $gameSystem.characterScaleDict()[key];
+            const scaleX     = scaleMoedl.x;
+            const scaleY     = scaleMoedl.y;
+            this.setCharacterScale(scaleX, scaleY, key);
+        });
+    };
+
+////=============================================================================
+//// Game_Scale
+////  倍率を保持するオブジェクト
+////=============================================================================
+
+    class Game_Scale {
+        constructor(scaleX = 1, scaleY = 1, symbol = Game_Scale.ALL_SYMBOL) {
+            this._x = scaleX;
+            this._y = scaleY;
+            this._symbol = symbol;
+        }
+
+        /**
+         * 「全ての要素」を表すシンボル名
+         */
+        static get ALL_SYMBOL() {
+            return 'all';
+        }
+
+        /**
+         * イベントのメモ欄で使用するメタタグ名
+         */
+        static get META_NAME() {
+            return 'scale';
+        }
+
+        get x() {
+            return this._x;
+        }
+
+        get y() {
+            return this._y;
+        }
+
+        get symbol() {
+            return this._symbol;
+        }
+
+        /**
+         * Scaleを設定する。
+         * @param {Number} x 
+         * @param {Number} y 
+         */
+        set(x = 1, y = 1) {
+            this._x = x;
+            this._y = y;
+        }
+    }
+    window[Game_Scale.name] = Game_Scale;
 
 })();
